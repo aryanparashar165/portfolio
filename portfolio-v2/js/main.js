6e212d7a0cb9f3fcb9774c44302f48cf378bfcc0
Aryadhiswar Parasar <70628682+aryanparashar165@users.noreply.github.com>
2026-06-08 23:38:11 +0530

    Update main.js

--- portfolio-v2/js/main.js
+++ portfolio-v2/js/main.js
@@ -1,1 +1,31 @@
+// Fade-in animation
 
+const observer = new IntersectionObserver(
+
+(entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},
+{
+    threshold:0.15
+}
+
+);
+
+document.querySelectorAll(".section")
+.forEach(section => {
+
+    section.classList.add("hidden");
+
+    observer.observe(section);
+
+});
